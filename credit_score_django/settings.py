24cd1b56af01c21c875d26c64f85fe5d5be87919
wendy <wendid263@gmail.com>
2026-05-24 19:00:04 +0700

    fix: add database config

--- credit_score_django/settings.py
+++ credit_score_django/settings.py
@@ -39,3 +39,10 @@ STATIC_URL = '/static/'
 
 import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
